c6754d029a631238602191b2b5ad197809558550
caroltc <312493732@qq.com>
2015-04-20 16:45:17 +0800

    Change-Id: I38d54647f6158fd9fe8d08088546183d3bbf2b71

---
+++ data/config/performance.php
@@ -0,0 +1,9 @@
+<?php 
+/* 性能设置 */
+return array (
+	'DEBUG'=>true,	//是否开启调试模式
+	'LOG_ON'=>true,	//是否开启出错信息保存到文件
+	'REWRITE_ON'=>false,	//伪静态开关
+	'COOKIE_PREFIX'=>'cI7c2_', // COOKIE前缀
+    'SAFE_KEY'=>'YLy2JjQaBxLBEHYl6OQp', // 加密码
+);
---
+++ data/config/upload.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    /* 上传设置 */
+    'UPLOAD_SIZE'=>100,
+    'UPLOAD_EXTS'=>'jpg,gif,bmp,png,doc,docx,ppt,pdf,pptx,xls,xlsx,rar,zip,7z,mp4,avi,flv',
+    'THUMB_STATUS'=>0,
+    'THUMB_TYPE'=>1,
+    'THUMB_WIDTH'=>800,
+    'THUMB_HEIGHT'=>800,
+    'WATER_STATUS'=>0,
+    'WATER_IMAGE'=>'logo.jpg',
+    'WATER_POSITION'=>1,
+);
